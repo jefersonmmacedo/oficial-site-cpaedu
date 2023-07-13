94b364206412beda88d4ccb7691333f10dcce588
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2023-07-12 23:41:15 -0300

    Update Partners.jsx

--- src/pages/Partners/Partners.jsx
+++ src/pages/Partners/Partners.jsx
@@ -1,12 +1,11 @@
 import "./partners.css";
-import uniBF from "../../assets/images/Icons/11.png"
-import prefeitura from "../../assets/images/Icons/22.png"
-import educaMais from "../../assets/images/Icons/33.png"
-import SaoLuiz from "../../assets/images/Icons/44.png"
-import UniBTA from "../../assets/images/Icons/55.png"
-import CPET from "../../assets/images/Icons/66.png"
+import UniBTA from "../../assets/images/Icons/9999.jpg"
 import unifacvet from "../../assets/images/Icons/77.png"
 import UniFVC from "../../assets/images/Icons/88.png"
+import saoLuiz from "../../assets/images/Icons/44.png"
+import CPET from "../../assets/images/Icons/66.png"
+import educanexus from "../../assets/images/Icons/33.png"
+import uniBF from "../../assets/images/Icons/11.png"
 import Navbar2 from "../../components/Nav/Navbar";
 import { Footer } from "../../components/Footer/Footer";
 
@@ -29,9 +28,6 @@ function Partners() {
             </div>
             <div className="image">
                 <img src={unifacvet} alt="Logo parceiro" />
-            </div>
-            <div className="image">
-                <img src={UniFVC} alt="Logo parceiro" />
             </div>
             <div className="image">
                 <img src={uniBF} alt="Logo parceiro" />
