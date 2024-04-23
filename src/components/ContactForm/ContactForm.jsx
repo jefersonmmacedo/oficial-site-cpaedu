80e37dd9bf5c591409d2592ed985195e097c2595
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2024-04-23 03:11:44 -0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -15,6 +15,9 @@ function ContactForm() {
     const [telefone, setTelefone] = useState("");
     const [mensagem, setMensagem] = useState("");
 
+    const [courses, setCourses] = useState([]);
+    const [course, setCourse] = useState("");
+
     useEffect(() => {
     async function loadCondadatos() {
         const querySnapshot = await getDocs(collection(db, "informations"));  
@@ -46,17 +49,58 @@ function ContactForm() {
     loadCondadatos()
 }, [])
 
-console.log(informations)
-console.log(informations[0])
+
+useEffect(() => {
+    async function loadCondadatos() {
+        const querySnapshot = await getDocs(collection(db, "courses"));  
+        const list = []
+        querySnapshot.forEach((doc) => {
+           // console.log(`${doc.id} => ${doc.data()}`);
+          const data = {
+            id: doc.id,
+            title: doc.data().title,
+            image: doc.data().image,
+            description: doc.data().description,
+            category: doc.data().category,
+            link: doc.data().link,
+            }
+            
+           // console.log(data)
+            list.push(data)
+          });
+          setCourses(list)
+    }
+
+    loadCondadatos()
+}, [])
+
 
 const phone = "22999942800"
-const text = `Olá, me chamo ${nome}! %0A${mensagem}. %0A%0AMeu telefone: ${telefone}. %0AMeu e-mail: ${email}.`
+const text = `Olá, me chamo ${nome}! %0A${mensagem}. %0A%0AMeu telefone: ${telefone}. %0AMeu e-mail: ${email}.  %0AMeu curso de interesse é: ${course}.`
 function handleNewMessage(e) {
     e.preventDefault();
     window.open("https://wa.me/55"+ phone + "?text=" + text,
     '_blank')
     toast.info("Mensagem enviada com sucesso")
   }
+
+  
+
+  if(courses) {
+    courses.sort(function(a,b) {
+        if(a.category < b.category ) {
+            return -1
+        } else {
+            return true
+        }
+    })
+    }
+
+    function handleSelectCourse(e){
+        console.log(e.target.value);
+        setCourse(e.target.value);
+    }
+
 
 
     return (
@@ -77,6 +121,13 @@ function handleNewMessage(e) {
                         <input type="text" name="" id="" placeholder="Nome"  value={nome} onChange={(e) => setNome(e.target.value)}/>
                         <input type="email" name="" id="" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         <input type="text" name="" id="" placeholder="Telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)}/>
+                        <select onChange={handleSelectCourse}>
+                            {courses?.map((course) => {
+                                return (
+                                    <option value={`${course.category} - ${course.title}`}>{`${course.category} - ${course.title}`}</option>
+                                )
+                            })}
+                        </select>
                         <textarea name="" id="" cols="30" rows="10" placeholder="Mensagem" value={mensagem} onChange={(e) => setMensagem(e.target.value)}></textarea>
                         <button onClick={handleNewMessage}>Enviar</button>
                 </form>
