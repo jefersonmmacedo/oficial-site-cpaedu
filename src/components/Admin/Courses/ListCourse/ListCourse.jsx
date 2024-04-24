a9beeef31d3095102608c740d76b3326c40894d3
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2024-04-24 14:13:59 -0300

    Update ListCourse.jsx

--- src/components/Admin/Courses/ListCourse/ListCourse.jsx
+++ src/components/Admin/Courses/ListCourse/ListCourse.jsx
@@ -101,7 +101,8 @@ function handleSearchCourse(e) {
 
 function handleUpdateCourse(course, category) {
   if(category === "Cursos Técnicos") {
-    window.open(`/adm/coursetecnic/${course}`, "_self")
+    window.open(`/adm/course/${course}/${category}`, "_self")
+    // window.open(`/adm/coursetecnic/${course}`, "_self")
   } else if (category === "Ensino Médio - EJA") {
     window.open(`/adm/courseejaedit/${course}`, "_self")
   } else{
